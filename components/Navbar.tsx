9f83c5dc28b066e210306992a085d74e9ff076b6
Cyrus <shantiravi@Mac.lan>
2026-07-21 19:41:40 +0530

    Enlarge logo symbol size inside containers by increasing scale to 2.2 in header and footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -55,9 +55,10 @@ export default function Navbar({ onDemoClick }: NavbarProps) {
                 alt="Eura Tech Solutions"
                 fill
                 sizes="112px"
-                className="object-contain object-center scale-[1.8]"
+                className="object-contain object-center scale-[2.2]"
                 priority
               />
+
 
             </div>
             {/* Geometric corner highlight indicators */}
